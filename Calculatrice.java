643c78225550c9e7595f25bc8056ed470a7da10f
Eldor <pan88kis@uni-duesseldorf.de>
2025-07-16 08:08:51 +0200

    Ajout de la méthode division

--- Calculatrice.java
+++ Calculatrice.java
@@ -2,4 +2,12 @@ public class Calculatrice {
     public int addition(int a, int b) {
         return a + b;
     }
+    
+    public int division(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division par zéro !");
+        }
+        return a / b;
+    }
+
 }
